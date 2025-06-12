fe743172596742b575f98687217e0712858b8891
shamsy-hassan <shamsyhassan254@gmail.com>
2025-06-12 22:07:55 +0300

    done

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,15 @@
 # your code goes here!
+# This one does NOT need time
+def countdown(n):
+    while n > 0:
+        print(f"{n} SECOND(S)!")
+        n -= 1
+    print("HAPPY NEW YEAR!")
+ 
+import time
+def countdown_with_sleep(n):
+    while n > 0:
+        print(f"{n} SECOND(S)!")
+        time.sleep(1)
+        n -= 1
+    print("HAPPY NEW YEAR!")
